abcdb12c4e53b7979cd8b21e954c8019c4cae242
Aleksander Panferov <panferov.a.a@yandex.ru>
2023-03-27 22:19:44 +0300

    fix: error test

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,87 +1,93 @@
 const Card = require("../models/card");
 
-const getcards = (req, res, next) => {
-  Card.find({})
-    .then((cards) => res.send({ data: cards }))
-    .catch(next);
+const getcards = async (req, res, next) => {
+  try {
+    const cards = await Card.find({});
+    res.send(cards);
+  } catch (err) {
+    next(err);
+  }
 };
 
-const delCard = (req, res, next) => {
+const delCard = async (req, res, next) => {
   const { id } = req.user._id;
-  Card.findByIdAndRemove(id)
-    .then((card) => res.send({ data: card }))
-    .catch((err) => {
-      if (err.name === "Not Found") {
-        res
-          .status(404)
-          .send({ message: `Карточка с указанным ${id} не найдена.` });
-      } else {
-        next(err);
-      }
-    });
+  try {
+    const card = await Card.findByIdAndRemove(id);
+    res.send(card);
+  } catch (err) {
+    if (err.name === "Not Found") {
+      res
+        .status(404)
+        .send({ message: `Карточка с указанным ${id} не найдена.` });
+    } else {
+      next(err);
+    }
+  }
 };
 
-const createcard = (req, res, next) => {
+const createcard = async (req, res, next) => {
   const { name, link } = req.body;
   const owner = req.user._id;
-
-  Card.create({ name, link, owner })
-    .then((card) => res.status(200).send(card))
-    .catch((err) => {
-      if (err.name === "ValidationError") {
-        res.status(400).send({
-          message: `Переданы некорректные данные при создании карточки. ${err}`,
-        });
-      } else {
-        next(err);
-      }
-    });
+  try {
+    const card = await Card.create({ name, link, owner });
+    res.status(200).send(card);
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      res.status(400).send({
+        message: `Переданы некорректные данные при создании карточки. ${err}`,
+      });
+    } else {
+      next(err);
+    }
+  }
 };
 
-const likeCard = (req, res, next) => {
-  Card.findByIdAndUpdate(
-    req.params.cardId,
-    { $addToSet: { likes: req.user._id } },
-    { new: true }
-  )
-    .then((card) => res.send({ data: card }))
-    .catch((err) => {
-      if (err.name === "ValidationError") {
-        res.status(400).send({
-          message: `Переданы некорректные данные для постановки лайка. ${err}`,
-        });
-      }
-      if (err.name === "Not Found") {
-        res
-          .status(404)
-          .send({ message: `Передан несуществующий ${id} карточки.` });
-      } else {
-        next(err);
-      }
-    });
+const likeCard = async (req, res, next) => {
+  try {
+    const card = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $addToSet: { likes: req.user._id } },
+      { new: true }
+    );
+    res.send(card);
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      res.status(400).send({
+        message: `Переданы некорректные данные для постановки лайка. ${err}`,
+      });
+    }
+    if (err.name === "Not Found") {
+      res
+        .status(404)
+        .send({ message: `Передан несуществующий ${id} карточки.` });
+    } else {
+      next(err);
+    }
+  }
 };
 
-const dislikeCard = (req, res, next) => {
-  Card.findByIdAndRemove(
-    req.params.cardId,
-    { $pull: { likes: req.user._id } },
-    { new: true }
-  )
-    .then((card) => res.send({ data: card }))
-    .catch((err) => {
-      if (err.name === "ValidationError") {
-        res.status(400).send({
-          message: `Переданы некорректные данные для снятии лайка. ${err}`,
-        });
-      }
-      if (err.name === "Not Found") {
-        res
-          .status(404)
-          .send({ message: `Передан несуществующий ${id} карточки.` });
-      } else {
-        next(err);
-      }
-    });
+const dislikeCard = async (req, res, next) => {
+  try {
+    const card = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $pull: { likes: req.user._id } },
+      { new: true }
+    );
+    res.send(card);
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      res.status(400).send({
+        message: `Переданы некорректные данные для снятии лайка. ${err}`,
+      });
+    }
+    if (err.name === "Not Found") {
+      res
+        .status(404)
+        .send({ message: `Передан несуществующий ${id} карточки.` });
+    } else {
+      next(err);
+    }
+  }
 };
 
 module.exports = { getcards, delCard, createcard, likeCard, dislikeCard };
--- controllers/users.js
+++ controllers/users.js
@@ -1,86 +1,90 @@
 const User = require("../models/user");
 
-const getUsers = (req, res, next) => {
-  User.find({})
-    .then((users) => res.send(users))
-    .catch(next);
+const getUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    next(err);
+  }
 };
 
-const getUserById = (req, res, next) => {
+const getUserById = async (req, res, next) => {
   const { id } = req.params;
-  User.find((item) => item._id === id)
-    .then((user) => res.send(user))
-    .catch((err) => {
-      if (err.name === "Not Found") {
-        res
-          .status(404)
-          .send({ message: `Пользователь по указанному ${id} не найден` });
-      } else {
-        next(err);
-      }
-    });
+  try {
+    const user = await User.find((item) => item._id === id);
+    res.send(user);
+  } catch (err) {
+    if (err.name === "Not Found") {
+      res
+        .status(404)
+        .send({ message: `Пользователь по указанному ${id} не найден` });
+    } else {
+      next(err);
+    }
+  }
 };
 
-const createUser = (req, res, next) => {
+const createUser = async (req, res, next) => {
   const { name, about, avatar } = req.body;
-
-  User.create({ name, about, avatar })
-    .then((user) => res.status(200).send(user))
-    .catch((err) => {
-      if (err.name === "ValidationError") {
-        res.status(400).send({
-          message: `Переданы некорректные данные при создании пользователя ${err}`,
-        });
-      } else {
-        next(err);
-      }
-    });
+  try {
+    const user = await User.create({ name, about, avatar });
+    res.status(200).send(user);
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      res.status(400).send({
+        message: `Переданы некорректные данные при создании пользователя ${err}`,
+      });
+    } else {
+      next(err);
+    }
+  }
 };
 
-const updProfile = (req, res, next) => {
+const updProfile = async (req, res, next) => {
   const { name, about } = req.body;
-
-  User.findByIdAndUpdate(
-    req.user._id,
-    { name: name, about: about },
-    { new: true, runValidators: true, upsert: true }
-  )
-    .then((user) => res.status(200).send(user))
-    .catch((err) => {
-      if (err.name === "ValidationError") {
-        res.status(400).send({
-          message: `Переданы некорректные данные при обновлении профиля. ${err}`,
-        });
-      }
-      if (err.name === "Not Found") {
-        res
-          .status(404)
-          .send({ message: `Пользователь по указанному ${id} не найден` });
-      } else {
-        next(err);
-      }
-    });
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      { name: name, about: about },
+      { new: true, runValidators: true, upsert: true }
+    );
+    res.status(200).send(user);
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      res.status(400).send({
+        message: `Переданы некорректные данные при обновлении профиля. ${err}`,
+      });
+    }
+    if (err.name === "Not Found") {
+      res
+        .status(404)
+        .send({ message: `Пользователь по указанному ${id} не найден` });
+    } else {
+      next(err);
+    }
+  }
 };
 
-const updAvatar = (req, res, next) => {
+const updAvatar = async (req, res, next) => {
   const { avatar } = req.body;
-
-  User.findByIdAndUpdate(req.user._id, { avatar: avatar })
-    .then((user) => res.status(200).send(user))
-    .catch((err) => {
-      if (err.name === "ValidationError") {
-        res.status(400).send({
-          message: `Переданы некорректные данные при обновлении аватара. ${err}`,
-        });
-      }
-      if (err.name === "Not Found") {
-        res
-          .status(404)
-          .send({ message: `Пользователь по указанному ${id} не найден` });
-      } else {
-        next(err);
-      }
-    });
+  try {
+    const user = await User.findByIdAndUpdate(req.user._id, { avatar: avatar });
+    res.status(200).send(user.avatar);
+  } catch (err) {
+    if (err.name === "ValidationError") {
+      res.status(400).send({
+        message: `Переданы некорректные данные при обновлении аватара. ${err}`,
+      });
+    }
+    if (err.name === "Not Found") {
+      res
+        .status(404)
+        .send({ message: `Пользователь по указанному ${id} не найден` });
+    } else {
+      next(err);
+    }
+  }
 };
 
 module.exports = { getUsers, getUserById, createUser, updProfile, updAvatar };
