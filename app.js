ef8e94fa8857d65efb2b4c7e7f7acb365ae394df
Aleksander Panferov <panferov.a.a@yandex.ru>
2023-06-12 20:25:51 +0300

    fix: fetch data

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes/index');
 // Слушаем 3000 порт
 const { PORT = 3000 } = process.env;
 const errorHandler = require('./middlewares/error-handler');
+const { requestLogger, errorLogger } = require('./middlewares/Logger');
 
 const app = express();
 
@@ -13,8 +15,23 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 });
 
 app.use(express.json());
+app.use(cors({
+  origin: [
+    'https://learning-mesto.nomoredomains.rocks',
+    'http://learning-mesto.nomoredomains.rocks',
+    'http://localhost:3000',
+    'http://158.160.102.19:3000',
+  ],
+  methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+}));
 app.use(express.urlencoded({ extended: false }));
+app.use(requestLogger);
 app.use(routes);
+app.use(errorLogger);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
